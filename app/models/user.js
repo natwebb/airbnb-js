ff0ccb1addb92e5aa05b93704fe7101ce26b6be9
Nat Webb <natrwebb@gmail.com>
2014-03-12 12:15:16 -0500

    register method on user

--- app/models/user.js
+++ app/models/user.js
@@ -2,8 +2,47 @@
 
 module.exports = User;
 
+var bcrypt = require('bcrypt');
+var users = global.nss.db.collection('users');
+var email = require('../lib/email');
+
 function User(data){
   this.role = data.role;
   this.email = data.email;
   this.password = data.password;
+}
+
+User.prototype.register = function(fn){
+  var self = this;
+
+  hashPassword(self.password, function(hashedPassword){
+    self.password = hashedPassword;
+    insert(self, function(err){
+      if(self._id){
+        email.sendWelcome({to:self.email}, function(err, body){
+          fn(err, body);
+        });
+      }else{
+        fn();
+      }
+    });
+  });
+};
+
+function hashPassword(password, fn){
+  bcrypt.hash(password, 8, function(err, hash){
+    fn(hash);
+  });
+}
+
+function insert(user, fn){
+  users.findOne({email:user.email}, function(err, record){
+    if(record){
+      fn(err);
+    }else{
+      users.insert(user, function(err, record){
+        fn(err);
+      });
+    }
+  });
 }
--- test/unit/user_spec.js
+++ test/unit/user_spec.js
@@ -2,8 +2,9 @@
 
 'use strict';
 
-process.env.DBNAME = 'bartertown-test';
+process.env.DBNAME = 'airbnb-test';
 var expect = require('chai').expect;
+var Mongo = require('mongodb');
 var User;
 //var fs = require('fs');
 //var exec = require('child_process').exec;
@@ -20,18 +21,47 @@ describe('User', function(){
 
   beforeEach(function(done){
     global.nss.db.dropDatabase(function(err, result){
-      done();
+      var u1 = new User({role:'host', email:'sue@nomail.com', password:'1234'});
+      u1.register(function(err){
+        done();
+      });
     });
   });
 
   describe('new', function(){
     it('should create a new User object', function(done){
-      var u1 = new User({role:'host', email:'adam@adam.com', password:'1234'});
+      var u1 = new User({role:'host', email:'bob@nomail.com', password:'1234'});
       expect(u1.role).to.equal('host');
-      expect(u1.email).to.equal('adam@adam.com');
+      expect(u1.email).to.equal('bob@nomail.com');
       expect(u1.password).to.equal('1234');
       done();
     });
   });
+
+  describe('#register', function(){
+    it('should register a user', function(done){
+      var u1 = new User({role:'host', email:'mocha-tests@hotmail.com', password:'1234'});
+      u1.register(function(err, body){
+        expect(err).to.not.be.ok;
+        expect(u1.password).to.have.length(60);
+        expect(u1._id).to.be.instanceof(Mongo.ObjectID);
+        body = JSON.parse(body);
+        expect(body.id).to.be.ok;
+        done();
+      });
+    });
+
+    it('should not register a user with duplicate email', function(done){
+      var u1 = new User({role:'host', email:'sue@nomail.com', password:'1234'});
+      u1.register(function(err){
+        expect(err).to.not.be.ok;
+        expect(u1.password).to.have.length(60);
+        expect(u1._id).to.be.undefined;
+        done();
+      });
+    });
+
+  });
+
 });
 
